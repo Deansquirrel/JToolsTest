9f6f867f8ffd1cd22b219889fe0bf4f5d1d0f5ba
yuansong6@163.com <yuansong6@163.com>
2020-12-23 18:45:32 +0800

    202012231845

---
+++ src/main/java/com/yuansong/tools/demo/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.yuansong.tools.demo.config;
+
+import org.springframework.stereotype.Component;
+
+import com.yuansong.tools.swagger.ISwaggerConfig;
+
+@Component
+public class SwaggerConfig implements ISwaggerConfig {
+
+	@Override
+	public Boolean getEnable() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getVersion() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getBasePackage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
--- src/main/java/com/yuansong/tools/demo/task/DemoTask.java
+++ src/main/java/com/yuansong/tools/demo/task/DemoTask.java
@@ -2,11 +2,12 @@ package com.yuansong.tools.demo.task;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.yuansong.tools.common.MathTool;
+import com.yuansong.tools.demo.config.SwaggerConfig;
 
 @Component
 @EnableScheduling
@@ -20,16 +21,21 @@ public class DemoTask {
 //		logger.debug("sub test");
 //	}
 	
+	@Autowired
+	private SwaggerConfig swaggerConfig;
+	
 	@Scheduled(initialDelay=1000L * 5, fixedDelay=1000L * 5)
 	public void toolTest() {
 		logger.debug("tool test begin");
-		int f = 0;
-		do {
-			logger.debug(String.valueOf(MathTool.RandInt(5, 7)));
-			f++;
-		}while(f < 10);
+		
+		logger.debug(String.valueOf(swaggerConfig.getEnable()));
+		logger.debug(swaggerConfig.getTitle());
+		logger.debug(swaggerConfig.getDescription());
+		logger.debug(swaggerConfig.getVersion());
+		
 		logger.debug("tool test end");
 	}
 	
 	
 }
+
