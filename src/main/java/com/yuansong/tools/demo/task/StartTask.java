e5d0e65a3ef73fdff5f49c5f47f25b9458b92072
yuansong6 <yuansong6@163.com>
2021-02-04 00:43:48 +0800

    202102040043

--- src/main/java/com/yuansong/tools/demo/task/StartTask.java
+++ src/main/java/com/yuansong/tools/demo/task/StartTask.java
@@ -6,32 +6,88 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import com.yuansong.tools.task.IDynamicTaskManager;
+import com.yuansong.tools.config.IToolsConfigHelper;
 
 @Component
 public class StartTask implements CommandLineRunner {
 	
-	private static final Logger logger = LoggerFactory.getLogger(StartTask.class);
-		
+//	private static final Logger logger = LoggerFactory.getLogger(StartTask.class);
+	
 	@Autowired
-	private IDynamicTaskManager dynamicTaskManager;
+	private IToolsConfigHelper toolsConfigHelper;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Thread t1 = new Thread(new MyRunnable(new Runnable() {
+			@Override
+			public void run() {
+				toolsConfigHelper.debug("TEST MESSAGE", "T1");
+			}
+		}, "T1"));
 		
+		Thread t2 = new Thread(new MyRunnable(new Runnable() {
+			@Override
+			public void run() {
+				toolsConfigHelper.debug("TEST MESSAGE", "T2");
+			}
+		}, "T2"));
+		
+		Thread t3 = new Thread(new MyRunnable(new Runnable() {
+			@Override
+			public void run() {
+				toolsConfigHelper.debug("TEST MESSAGE", "T3");
+			}
+		}, "T3"));
+		
+		Thread t4 = new Thread(new MyRunnable(new Runnable() {
+			@Override
+			public void run() {
+				toolsConfigHelper.debug("TEST MESSAGE", "T4");
+			}
+		}, "T4"));
+		
+		Thread.sleep(3000);
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		
+	}
+}
 
+class MyRunnable implements Runnable {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MyRunnable.class);
+	
+	public MyRunnable(Runnable r, String type) {
+		this.r = r;
+		this.type = type;
 	}
 	
-//	private void printS() {
-//		for(String name : dynamicTaskManager.getTaskList()) {
-//			logger.debug(MessageFormat.format("task {0} {1} {2} {3}", 
-//					name, 
-//					dynamicTaskManager.isRunning(name),
-//					dynamicTaskManager.isStarted(name),
-//					dynamicTaskManager.containTask(name)
-//					));
-//			
-//		}
-//	}
+	private Runnable r;
+	private String type;
 
+	@Override
+	public void run() {
+		boolean flag = true;
+		long count = 0L;
+		long c = 0L;
+		while(flag) {
+			this.r.run();
+			if(count > 50) {
+				c = c + 1;
+				count = 0;
+				logger.debug(this.type + " " + String.valueOf(c));
+				try {
+					Thread.sleep(100);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} else {
+				count++;
+			}
+		}
+	}
+	
 }
