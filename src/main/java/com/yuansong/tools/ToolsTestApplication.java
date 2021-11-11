bfc2b375f9c1e3cb695174997b496190756370f4
yuansong6 <yuansong6@163.com>
2021-11-12 01:00:55 +0800

    202111120100

---
+++ src/main/java/com/yuansong/tools/ToolsTestApplication.java
@@ -0,0 +1,12 @@
+package com.yuansong.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToolsTestApplication.class, args);
+    }
+}
--- src/main/java/com/yuansong/tools/demo/global/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.yuansong.tools.demo.global;
-
-public class Constant {
-	
-	public static final String DB_TEST = "DB_TEST";
-
-}
