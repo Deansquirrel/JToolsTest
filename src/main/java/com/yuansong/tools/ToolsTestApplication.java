1fd7d56f191e8bab6f922827eb5bcc87a600cf98
yuansong6 <yuansong6@163.com>
2021-11-14 23:14:06 +0800

    202111142313

--- src/main/java/com/yuansong/tools/ToolsTestApplication.java
+++ src/main/java/com/yuansong/tools/ToolsTestApplication.java
@@ -2,11 +2,36 @@ package com.yuansong.tools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.github.deansquirrel","com.yuansong" })
+@EnableScheduling
+@EnableAsync
 public class ToolsTestApplication {
 
     public static void main(String[] args) {
+
+        long begTime = System.currentTimeMillis();
+
         SpringApplication.run(ToolsTestApplication.class, args);
+
+        long endTime = System.currentTimeMillis();
+
+        System.out.println("\n"
+                + "-----------------------------------------------------------------------\n"
+                + "-----------------------------------------------------------------------\n"
+                + "                               _     _\n"
+                + "                              ( \\---/ )\n"
+                + "                               ) . . (\n"
+                + "-------------------------,--._(___Y___)_,--.---------------------------\n"
+                + "                         `--'           `--'\n"
+                + "                     	 JToolsTest  启动成功                   \n"
+                + "\n" + " 启动耗时: " + (endTime - begTime)
+                + "毫秒 \n"
+                + "-----------------------------------------------------------------------\n"
+                + "-----------------------------------------------------------------------\n");
+
     }
+
 }
